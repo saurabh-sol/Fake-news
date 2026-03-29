e7a9d951b8b6c861602195281f15c019b81ceb81
XROH-io <vikashsharma191619@gmail.com>
2026-03-30 00:35:31 +0530

    fix: update CORS for Vercel deployment

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # During development, or specify http://localhost:3000
+    allow_origins=[
+        "http://localhost:3000",
+        "https://fake-news-mauve.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
